ce4373a093b58185bc455e1b8acbd7918fe45167
Fogelman <ddfogelman@gmail.com>
2018-10-12 13:01:43 -0300

    Plotando os gráficos durante a emissão e refatorando o receive

--- receive.py
+++ receive.py
@@ -4,44 +4,49 @@ import signalTeste
 import numpy as np
 import sounddevice as sd
 
-
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy import signal as window
 import peakutils
+import json
 
 
+
+with open ("data.json") as json_file:
+    data = json.load(json_file)
 
 signal = signalTeste.signalMeu()
 fs= 44100
-
 duration = 2  # seconds
-myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
+freq = data["freq"]
 
-sd.wait()
+def record (duration, fs):
+    print("iniciando a gravação")
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
+    sd.wait()
 
-sd.play(myrecording,fs)
-print(myrecording)
-
-sd.wait()
-
-
-myrecording = myrecording[:,0]
-print(len(myrecording))
-print(myrecording)
-# signal.plotFFT(myrecording,fs)
-x,y = signal.calcFFT(myrecording, fs)
-indexes = peakutils.indexes(y, thres=0.5, min_dist=30)
-peaks_x = peakutils.interpolate(x, y, ind=indexes)
-print(peaks_x)
-print(x[indexes], y[indexes])
-
-point = [x[indexes][0],y[indexes][0]]
-plt.figure()
-plt.plot(x, y)
-plt.plot(x[indexes][0],y[indexes][0])    
-plt.show()
-plt.title('Fourier')
+    print(len(myrecording))
+    print(myrecording)
+    return myrecording[:,0] #return só de um canal do recording
 
 
+def play_recording (recording, fs):
+    sd.play(recording,fs)
+    sd.wait()
 
+def plotFFT(x,y):
+    indexes = peakutils.indexes(y, thres=0.5, min_dist=30)
+    print(x[indexes], y[indexes])
+
+    # point = [x[indexes][0],y[indexes][0]]
+    plt.figure()
+    plt.plot(x, y)
+    for i in range(len(x[indexes])):
+        plt.plot(x[indexes][i],y[indexes][i],'ro')  
+    plt.show()
+    plt.title('Fourier')
+
+recording = record(duration,fs)
+x,y = signal.calcFFT(recording, fs)
+# play_recording(recording,fs)
+plotFFT(x,y)
--- send.py
+++ send.py
@@ -4,35 +4,81 @@ import signalTeste
 import numpy as np
 import sounddevice as sd
 
+import matplotlib.pyplot as plt
 import json
 
-freq = {
-    "1":[697,1209],
-    "2":[697,1336],
-    "3":[697,1477],
-    "4":[770,1209],
-    "5":[770,1336],
-    "6":[770,1477],
-    "7":[852,1209],
-    "8":[852,1336],
-    "9":[852,1477],
-    "0":[941,1336],
 
-}
+with open ("data.json") as json_file:
+    data = json.load(json_file)
+
+
+freq = data["freq"]
+
 amplitude = 0.5
 periodo = 1 #Em segundos
 fs = 44100
 signal = signalTeste.signalMeu()
+while True:
+    print("Digite um número:", end=" ")
+    digito = input("")
+    
+    if (digito.isdigit() and len(digito) == 1 ):
+        plt.close("all") 
+        total = []
+        t1, x1 = signal.generateSin(freq[digito][0],amplitude,periodo,fs)
+        t2, x2 = signal.generateSin(freq[digito][1],amplitude,periodo,fs)
+        
+        total = x1 +x2
+        
+        plt.figure("Digito {0}".format(digito))
+        plt.title('Senoide de {0} hz'.format(freq[digito][0]), fontsize=8)
+        ax1 = plt.subplot(311)
+        plt.plot(t1, x1)
+        plt.title('Senoide de {0} hz'.format(freq[digito][0]), fontsize=8)
+        plt.ylabel('Amplitude', fontsize=8)
 
-digito = input("Digite um numero: ")
-total = []
+        plt.setp(ax1.get_xticklabels(), fontsize=6)
+
+        # share x only
+        ax2 = plt.subplot(312, sharex=ax1, sharey=ax1)
+        plt.plot(t2, x2)
+        plt.title('Senoide de {0} hz'.format(freq[digito][1]), fontsize=8)
+        plt.ylabel('Amplitude', fontsize=8)
+
+        # make these tick labels invisible
+        plt.setp(ax2.get_xticklabels(), fontsize=6)
+
+        # share x and y
+        ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
+        plt.plot(t1, total)
+        plt.title('Somatória das senoides de {0} hz e {1} hz'.format(freq[digito][0],
+        freq[digito][1]), fontsize=8)
+        plt.ylabel('Amplitude', fontsize=8)
+        
+        plt.setp(ax3.get_xticklabels(), fontsize=6)
+
+        plt.xlabel('Tempo (Segundos)', fontsize=8)
+        plt.xlim(0.0, 0.025)
+        plt.tight_layout()
+        plt.show(block=False)
+
+        sd.play(total, fs)
+        sd.wait()
+
+        
+        
+        
+        
+        
+        
+        
 
 
-x1 = signal.generateSin(freq[digito][0],amplitude,periodo,fs)[1]
-x2 = signal.generateSin(freq[digito][1],amplitude,periodo,fs)[1]
 
-total = x1 +x2
 
-sd.play(total, fs)
-sd.wait()
+
+
+
+
+
 
