7955b7211d34ccb6871274170a662667c3b2289a
mariaeduardabicalho <mariaeduardabicalho@gmail.com>
2018-11-08 12:24:24 -0200

    msgns separdas funcionando

--- MOD_DEMOD/socketreceive.py
+++ MOD_DEMOD/socketreceive.py
@@ -6,10 +6,14 @@ s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig= (host,port)
 s.bind(orig)
 s.listen(1)
-
+mensagem =[]
 while True:
 	con, cliente = s.accept()
 	while True:
 		msg = con.recv(1024)
+		print(msg)
+		mensagem.add(msg)
 		if not msg: break
+		print(cliente, mensagem)
+	print("finalizando conexao do cliente", cliente)
 	con.close()
